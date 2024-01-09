fbe16524ee609db5d67c48e5a68fda19744fbd24
“raghava104” <raghavachakra24@gmail.com>
2024-01-09 19:51:58 +0530

    5thday work done

--- myjava.java
+++ myjava.java
@@ -3,4 +3,12 @@ This is second line by DevB
 This is third line by DevA
 this is new edited line at 9thjan at 7.43pm
 this is edited line at 9thjan 7.44pm
+<<<<<<< Updated upstream
 this is my manager work
+=======
+this is first day of java completed
+this is second lines of java completed
+this is third day of java lines completed
+this is fourth lines of java completed
+this is fifth day of java completed:
+>>>>>>> Stashed changes
